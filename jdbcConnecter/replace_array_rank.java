e19e3fe634299e14af2442b73d70e6323e622e40
Tushar Baranwal <tusharbaranwal123@gmail.com>
2023-07-14 17:05:42 +0530

    hjk

--- jdbcConnecter/replace_array_rank.java
+++ jdbcConnecter/replace_array_rank.java
@@ -1,8 +1,27 @@
 package jdbcConnecter;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class replace_array_rank {
     public static void main(String[] args) {
-        
+        int array[] = {9,4,5,7,2,8,1};
+
+        int sortedarr[] = Arrays.copyOf(array,array.length);
+
+        Arrays.sort(sortedarr);
+
+        HashMap <Integer,Integer> rankMap = new HashMap();
+        for(int i=0;i<sortedarr.length;i++){
+            rankMap.put(sortedarr[i],i+1);
+
+        } 
+        for(int i=0;i<array.length;i++){
+            array[i] = rankMap.get(array[i]);
+        }
+        System.out.println(Arrays.toString(array));
+      
+    
     }
 
 }
