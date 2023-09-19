50db5ff7b9d18d6c4b27ab334fc2f50f60a27347
Tushar Baranwal <tusharbaranwal123@gmail.com>
2023-09-19 16:32:31 +0530

    update

--- jdbcConnecter/greater_right_side.java
+++ jdbcConnecter/greater_right_side.java
@@ -1,17 +1,23 @@
 package jdbcConnecter;
 
+import java.util.Arrays;
+
 public class greater_right_side {
+    
     public static void main(String[] args) {
         int arr[] = {16, 17, 4, 3, 5, 2};
         int n=6;
 
-        arr[n-1] = -1;
-        for(int i=n-1;i>=0;i--){
-            int temp = arr[i];
-            arr[i] = arr[n-1];
-            arr[n-1] = Math.max(arr[n-1], temp);
+       int max = arr[n-1];
+       arr[n-1] =-1;
 
-        }
+       for(int i=n-2;i>=0;i--){
+        int temp=arr[i];
+        arr[i] = max;
+        if(temp>max)
+        max = temp;
+       }
+       System.out.println(Arrays.toString(arr));
         
     }
     
